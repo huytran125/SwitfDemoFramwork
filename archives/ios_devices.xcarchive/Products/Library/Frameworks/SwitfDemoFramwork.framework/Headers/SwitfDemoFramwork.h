097d890a31a99618ef0d632f86a07f25bbc3a6d3
Tran Quang Huy <>
2021-06-21 10:00:11 +0700

    my first commit

---
+++ archives/ios_devices.xcarchive/Products/Library/Frameworks/SwitfDemoFramwork.framework/Headers/SwitfDemoFramwork.h
@@ -0,0 +1,18 @@
+//
+//  SwitfDemoFramwork.h
+//  SwitfDemoFramwork
+//
+//  Created by Tran Quang Huy on 6/21/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwitfDemoFramwork.
+FOUNDATION_EXPORT double SwitfDemoFramworkVersionNumber;
+
+//! Project version string for SwitfDemoFramwork.
+FOUNDATION_EXPORT const unsigned char SwitfDemoFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwitfDemoFramwork/PublicHeader.h>
+
+
